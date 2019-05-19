cd5bef516f70af33e9f0e0f194b69b77621d57bf
Yusuke Kumura <yusuke.kumura@gmail.com>
2019-05-19 09:45:16 +0900

    Remove Duplicates (easy)

--- TwoPointersPattern.py
+++ TwoPointersPattern.py
@@ -28,7 +28,8 @@ def remove_duplicates(arr):
     Remove Duplicates (easy)
     Given an array of sorted numbers, remove all duplicates from it. You should not use any extra space;
     after removing the duplicates in-place return the new length of the array.
-
+    Time: O(N)
+    Space: O(1), this is in-place algorithm
     :param arr:
     :return:
     """
